103156b0490e197c2b74cfb5aa912700ac5e42a8
Julio Abarca <juliotec2@hotmail.com>
2022-10-14 15:57:08 -0500

    Changes

--- ExpressionXmlSerializer/Program.cs
+++ ExpressionXmlSerializer/Program.cs
@@ -6,7 +6,7 @@ namespace ExpressionTools
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             var expressionXmlSerializer = new ExpressionXmlSerializer();
 
